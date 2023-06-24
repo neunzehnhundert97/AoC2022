94515dfde9e8695e2f10eb02303a867103f3825e
Leon A. Becker <leonalexbecker@yahoo.de>
2023-06-24 23:21:10 +0200

    Document helpers

--- ocaml/shared.ml
+++ ocaml/shared.ml
@@ -1,7 +1,10 @@
-(** Read all content of a given [filename]. *)
+(** Read all content of a given [filename] and return a list of lines. *)
 let read_file filename =
   In_channel.(with_open_text filename input_all) |> Str.split (Str.regexp "\n")
 
+(** Print a list of strings.*)
 let print_string_list list = List.iter print_string list
+(** Function application with lower priority to get around parens. *)
 let ( $ ) f x = f x
+(** And then operator.*)
 let ( >> ) f g x = g (f x)
