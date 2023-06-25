ec0a15018848321de1472daf11a80397f312acbd
Leon A. Becker <leonalexbecker@yahoo.de>
2023-06-25 16:37:50 +0200

    Redo docstrings

--- ocaml/shared.ml
+++ ocaml/shared.ml
@@ -4,7 +4,9 @@ let read_file filename =
 
 (** Print a list of strings.*)
 let print_string_list list = List.iter print_string list
+
 (** Function application with lower priority to get around parens. *)
 let ( $ ) f x = f x
+
 (** And then operator.*)
 let ( >> ) f g x = g (f x)
