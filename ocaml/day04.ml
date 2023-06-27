5bf73aeb6abb0a9199b60f69495484ec4bc4fcf4
Leon A. Becker <leonalexbecker@yahoo.de>
2023-06-27 18:50:47 +0200

    Prepare day 4

---
+++ ocaml/day04.ml
@@ -0,0 +1,13 @@
+open Shared
+module IntSet = Set.Make (Int)
+
+let solve1 lines = List.length lines
+
+let _ =
+  let example_data = read_file "example04.data" in
+  let data = read_file "input04.data" in
+  print_endline "== Day 04 ==";
+  print_endline "Example 1\n";
+  if solve1 example_data = 2 then print_endline "Solved correctly"
+  else failwith "Example failed";
+  data |> solve1 |> Printf.printf "Solution 1: %d\n"
